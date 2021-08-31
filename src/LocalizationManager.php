517d88b5f51acd4d95ebb64088aaf93ac52f2779
Yuriy Martini <y.martini@soluzionesoftware.com>
2021-08-31 10:10:13 +0200

    added locale negotiation from request headers

--- src/LocalizationManager.php
+++ src/LocalizationManager.php
@@ -4,6 +4,7 @@ namespace SoluzioneSoftware\Localization;
 
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Request;
 use Illuminate\Support\Str;
 use SoluzioneSoftware\Localization\Facades\Route;
 
@@ -15,12 +16,35 @@ class LocalizationManager
 
         return in_array($prefix, $this->all())
             ? $prefix
-            : App::getLocale();
+            : ($this->negotiateFromHeaders() ?: App::getLocale());
     }
 
     public function all(): array
     {
         return Config::get('localization.locales');
+    }
+
+    private function negotiateFromHeaders(): ?string
+    {
+        $accept = Request::header('Accept-Language');
+        if (!$accept) {
+            return null;
+        }
+
+        $locales = $this->all();
+        foreach (explode(';', $accept) as $item) {
+            if (in_array($item, $locales)) {
+                return $item;
+            }
+
+            foreach (explode(',', $item) as $item2) {
+                if (in_array($item2, $locales)) {
+                    return $item2;
+                }
+            }
+        }
+
+        return null;
     }
 
     public function localizeRouteName(string $routeName, ?string $locale = null): string
